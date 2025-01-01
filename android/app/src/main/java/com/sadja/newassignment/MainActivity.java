d01e0207e96836cb49fc65ae22f49fcfb025f8fd
naman mishra <namanmishraec1045@gmail.com>
2025-01-01 12:21:11 +0530

    Updated:

---
+++ android/app/src/main/java/com/sadja/newassignment/MainActivity.java
@@ -0,0 +1,5 @@
+package com.sadja.newassignment;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
-  appName: 'assignment',
+  appId: 'com.sadja.newassignment',
+  appName: 'newassignment',
   webDir: 'www',
   // server: {
   //   url: 'http://10.0.2.2:8100', // Replace with your host machine IP
